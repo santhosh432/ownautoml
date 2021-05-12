012cf03dfa147d627368a6d60079cb7de6363f0e
santhosh <santhoshcusat17@gmail.com>
2021-05-12 17:43:01 +0530

    src : default create_folds and config files created

---
+++ ai/own/src/default/config.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+import os
+
+TRAINING_FILE = '../../input/train_folds.csv'
+
+MODELS = '../models/'
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+print('DIR', DIR)
+
+
+def input_data(file_name='mnist_train.csv', project=None):
+    """
+    :param file_name: input CSV file name with all semesters.
+    :return: full pah
+    """
+    # DIR = DIR
+    p = DIR + '/{0}'.format(file_name)
+    p = p.replace('src', 'input')
+    print('input data', p)
+
+    return p
+
+
+def models_path(project):
+
+    p = DIR + '/{0}/'.format(project)
+    p = p.replace('src', 'models')
+
+    return p
+
+
+def training_file():
+
+    p = DIR + '/{0}'.format('train_fold.csv')
+    p = p.replace('src', 'input')
+    print('input data', p)
+    return p
+
+
+if __name__ == '__main__':
+    # input_data('FinalMarksGPA-2021-04-15(1).csv')
+    # models_path()
+    pass
+
