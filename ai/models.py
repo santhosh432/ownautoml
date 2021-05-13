6d5cf1cf8486e017d28a5fe3d6b9ce6c0307ac75
santhosh <santhoshcusat17@gmail.com>
2021-05-13 15:44:36 +0530

    models: data file path filed created for model

--- ai/models.py
+++ ai/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-
+import os
 # Create your models here.
+
+
+def user_directory_path(instance, filename='train_data.csv'):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    p = os.getcwd()
+    path = 'ai/own/input/' + instance.project_name + '/' + filename
+    return path
 
 
 class Project(models.Model):
@@ -10,6 +17,7 @@ class Project(models.Model):
                     )
     project_name = models.CharField(max_length=150, verbose_name='Project name')
     project_type = models.CharField(max_length=1, choices=PROJECT_TYPE)
+    data = models.FileField(null=True, blank=True, upload_to=user_directory_path)
 
     def __str__(self):
         return "{0} - {1}".format(self.project_name, self.project_type)
