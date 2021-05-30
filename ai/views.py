d47d04b5ef72f383490052b16fa0d5e50494feac
santhosh <santhoshcusat17@gmail.com>
2021-05-30 19:37:29 +0530

    views: with dynamic  forms

--- ai/views.py
+++ ai/views.py
@@ -16,17 +16,10 @@ def main_predict(request, pk):
     project = Project.objects.get(pk=pk)
 
     path = os.getcwd() + '/ai/own/input/{0}/train_fold.csv'.format(project.project_name.lower())
-    # path = os.getcwd() + '/train_fold.csv'.format(project.project_name.lower())
-    print(path)
     df = pd.read_csv(path)
     df = df.sample(5)
 
-    # print(df.columns.values)
-    # print(df.dtypes.to_dict())
-
     column_filter = {k: str(v) for k, v in df.dtypes.to_dict().items() if k not in ['Unnamed: 0', 'kfold', 'label']}
-
-    print(column_filter)
 
     def fcolumn(c):
         d = {}
@@ -47,34 +40,20 @@ def main_predict(request, pk):
 
     form = type('DynamicItemsForm', (MyForm,), fcolumn(column_filter))
 
-    print('main form:', form)
-    print('main form:', dir(form))
-    # print('main form:', form.as_p())
-
     p = None
 
-    # print(ClassificationReport.objects.filter(project_name__id=pk, activate=True))
     model_selection = ClassificationReport.objects.filter(project_name__id=pk, activate=True).first()
-    # print(model_selection.model_path)
 
     if request.GET:
 
-        path_model = os.getcwd() + '/ai/own/src/{0}'.format(project.project_name)
-        # os.chdir(path_model)
-        # sys.path.append(path_model)
-
-        # import inference
-
         form = form(request.GET)
         data = dict(request.GET)
-        # print('request data', data)
         data.pop('predict', None)
-        # p = inference.predict(data, '/model-DTEC,score-75.0,fold-4.bin')[0]
         p = inference.predict(data, model_selection.model_path, project_name=project.project_name)[0]
-        # sys.path.remove(path_model)
+
     print('Final predict:', p)
     return render(request, 'ai/predict.html', context={'form': form,
-                                                        'p': p,
-                                                        'project': project,
-                                                        'df': df.to_html()
-                                                        })
+                                                       'p': p,
+                                                       'project': project,
+                                                       'df': df.to_html()
+                                                       })
