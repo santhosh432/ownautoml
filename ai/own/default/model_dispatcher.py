862a30115f36ae938062ca910ef0a7f7af75eb34
santhosh <santhoshcusat17@gmail.com>
2021-05-30 17:43:12 +0530

    default: model dispatcher with other models

--- ai/own/default/model_dispatcher.py
+++ ai/own/default/model_dispatcher.py
@@ -1,7 +1,16 @@
 from sklearn import tree
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier,\
+    GradientBoostingRegressor, AdaBoostRegressor, StackingRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, ElasticNet, LassoLars, Lasso, RidgeCV
+import xgboost as xgb
+from sklearn.svm import SVR
+
 
 models = {
-    'DTGC': tree.DecisionTreeClassifier(criterion='gini'),
-    'DTEC': tree.DecisionTreeClassifier(criterion='entropy'),
+    'DTGC': tree.DecisionTreeClassifier(criterion='gini', random_state=42),
+    'DTEC': tree.DecisionTreeClassifier(criterion='entropy', random_state=42),
+    "RF": RandomForestClassifier(random_state=42),
+    "XGB": xgb.XGBClassifier(random_state=42),
 
 }
