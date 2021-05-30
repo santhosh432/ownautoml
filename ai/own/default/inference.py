edc69ed9d8f9bf9722e77a147ce3166f2d0d8fcc
santhosh <santhoshcusat17@gmail.com>
2021-05-30 17:42:43 +0530

    default: inference with project as arg

--- ai/own/default/inference.py
+++ ai/own/default/inference.py
@@ -5,10 +5,13 @@ import pandas as pd
 DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def predict(data, model):
+def predict(data, model, project_name=None):
     model_path = DIR.replace('src', 'models')
-    print(model_path)
+    # print(model_path)
     model_path = model_path + model
+    # print('default:', model_path)
+
+    model_path = model_path.replace('default', 'models/{0}'.format(project_name))
     m = joblib.load(model_path)
 
     return m.predict(pd.DataFrame(data))
